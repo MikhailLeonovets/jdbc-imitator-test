ce24a3e501823c7dc136f518f1a03f2450968729
Mikhail Leonovets <mikhail.leonovets@gmail.com>
2022-12-12 16:01:12 +0300

    initial commit

---
+++ src/main/java/com/leonovets/jdbcimitatortest/JdbcImitatorTestApplication.java
@@ -0,0 +1,13 @@
+package com.leonovets.jdbcimitatortest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JdbcImitatorTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JdbcImitatorTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/leonovets/jdbcimitatortest/JdbcImitatorTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leonovets.jdbcimitatortest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JdbcImitatorTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
